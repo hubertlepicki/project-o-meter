50e38b1ee24d5dcd6838a8fa59c5c2ac15924959
Hubert <hubert.lepicki@amberbit.com>
2010-08-14 12:29:49 +0200

    more test stubs

---
+++ spec/unit/metric_spec.rb
@@ -0,0 +1,6 @@
+describe Metric
+  it "should have array of points in time to collect metrics"
+
+  it "should iterate over points in time to collect metrics"
+
+end
---
+++ spec/unit/project_spec.rb
@@ -0,0 +1,7 @@
+describe Project
+  it "should have name"
+  it "should have repository type"
+  it "should have repository url"
+  it "should have list of metrics"
+  it "should collect list of metrics"
+end
