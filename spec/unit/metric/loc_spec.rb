fb3d95bdd9163255e7486ce5a880e01dafbc78e5
Hubert <hubert.lepicki@amberbit.com>
2010-08-22 16:55:14 +0200

    minor formatting fix

--- spec/unit/metric/loc_spec.rb
+++ spec/unit/metric/loc_spec.rb
@@ -5,8 +5,8 @@ describe LocMetric do
      example_config_for_project
      @config.metrics = [OpenStruct.new(:name => "loc", :dates => ["2010-08-13 00:00:00", "2010-08-14 14:01:00", "2010-08-15 00:00:00"])]
 
-    @project = ProjectOMeter.instance("/some/path.yml")
-    @project.collect_metrics
+     @project = ProjectOMeter.instance("/some/path.yml")
+     @project.collect_metrics
    end
 
   it "should have 10 locs changed in first period" do
@@ -16,6 +16,5 @@ describe LocMetric do
   it "should have 5 loc changed in second period" do
     LocMetric.first(:sort => ["from", "ascending"], :skip => 5).value.should eql(1.0)
   end 
-
 
 end
